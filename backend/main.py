7a5b708e2258b958e6d6b6904cba8b09033522fc
liubrian267 <liubrian267@gmail.com>
2024-10-12 17:16:28 -0700

    item search

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ import uvicorn
 from dotenv import load_dotenv
 import os
 from urllib.parse import urlencode
+import json
 
 # Load the .env file
 load_dotenv()
@@ -12,16 +13,15 @@ load_dotenv()
 # API_KEY = os.getenv('APP_ID')
 app = FastAPI()
 
-baseURL ='https://openexchangerates.org/api/latest.json'
-base = 'USD'
-access_key = os.getenv('APP_ID')
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 @app.get("/test")
 async def read_item():
+    baseURL ='https://openexchangerates.org/api/latest.json'
+    base = 'USD'
+    access_key = os.getenv('APP_ID')
     params = {
         "app_id": access_key,  # The API key
         "symbols": "EUR,GBP,CAD,PLN,JPY,CNY"  # The currency symbols to fetch
@@ -35,6 +35,16 @@ async def read_item():
     if response.status_code != 200:
         return {'error': 'Failed to fetch data'}
     return response.json()
+
+@app.get("/item/{item}")
+def retrieve_item(item: str):
+    response = requests.get('https://fakestoreapi.com/products')
+    products = response.json()
+    filtered_products = [
+        product for product in products if item.lower() in product['title'].lower()
+    ]
+    
+    return filtered_products
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
